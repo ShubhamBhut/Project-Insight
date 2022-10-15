6e5faa557680d61183d5d8549cab24b6c5158270
Shubham Patel <shubhamprecious1@gmail.com>
2022-10-15 16:00:13 +0530

    Update pantheon_web.py

--- pantheon_web.py
+++ pantheon_web.py
@@ -6,7 +6,8 @@ import streamlit as st
 st.title("Project Insight")
 
 st.header("Select the timeline")
-st.selectbox('',['All', 'Preclassical Era', 'Classical Era', 'Medievel Era', 'Early modern Era', 'Era of Revolutions', 'Information Era'])
-
+#timeline = st.selectbox('',['All', 'Preclassical Era', 'Classical Era', 'Medievel Era', 'Early modern Era', 'Era of Revolutions', 'Information Era'])
+timeline = st.multiselect('',['All', 'Preclassical Era', 'Classical Era', 'Medievel Era', 'Early-Modern Era', 'Era of REvolutions', 'Information Era'])
+#st.write('The selected timeline is ', timeline)
 st.sidebar.header("Project Insight")
 
