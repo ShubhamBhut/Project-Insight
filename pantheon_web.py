97fb854b7c27574e2bbde1893f79a36a0650ec2b
Shubham Patel <shubhamprecious1@gmail.com>
2022-10-15 15:40:44 +0530

    Update pantheon_web.py

--- pantheon_web.py
+++ pantheon_web.py
@@ -3,4 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.write("Hello world")
+st.title("Project Insight")
+
+st.header("Select the timeline")
+st.selectbox('',['All', 'Preclassical Era', 'Classical Era', 'Medievel Era', 'Early modern Era', 'Era of Revolutions', 'Information Era'])
+
+st.sidebar.header("Project Insight")
+
